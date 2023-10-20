3eee4eba42bff275a10d057531a73673193de6d4
Kirat Bains <kiratbains09@gmail.com>
2023-10-19 20:15:00 -0700

    feat: dependency update

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import csv
 import os
 import click
+from babel.numbers import format_currency
 
 # Define the filename for the expense data
 expense_file = 'expenses.csv'
+categories_file = 'categories.csv'
 
-# Create the CSV file if it doesn't exist
+# Create the CSV files if they don't exist
 if not os.path.exists(expense_file):
     with open(expense_file, 'w', newline='') as csvfile:
-        fieldnames = ['Date', 'Description', 'Amount']
+        fieldnames = ['Date', 'Description', 'Amount', 'Category']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+if not os.path.exists(categories_file):
+    with open(categories_file, 'w', newline='') as csvfile:
+        fieldnames = ['Category']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
 
@@ -20,22 +28,42 @@ def cli():
 @click.option('--date', prompt='Date (YYYY-MM-DD)', help='Expense date')
 @click.option('--description', prompt='Description', help='Expense description')
 @click.option('--amount', prompt='Amount', type=float, help='Expense amount')
-def add(date, description, amount):
+@click.option('--category', prompt='Category', help='Expense category')
+def add(date, description, amount, category):
     """Add a new expense"""
     with open(expense_file, 'a', newline='') as csvfile:
-        fieldnames = ['Date', 'Description', 'Amount']
+        fieldnames = ['Date', 'Description', 'Amount', 'Category']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writerow({'Date': date, 'Description': description, 'Amount': amount})
-    click.echo(f'Expense added: Date: {date}, Description: {description}, Amount: {amount}')
+        writer.writerow({'Date': date, 'Description': description, 'Amount': amount, 'Category': category})
+    click.echo(f'Expense added: Date: {date}, Description: {description}, Amount: {format_currency(amount, "USD", locale="en_US")}, Category: {category}')
 
 @cli.command()
-def list():
-    """List all expenses"""
+@click.option('--date', prompt='Date to edit (YYYY-MM-DD)', help='Date of the expense to edit')
+@click.option('--description', prompt='New description', help='New description')
+@click.option('--amount', prompt='New amount', type=float, help='New amount')
+@click.option('--category', prompt='New category', help='New category')
+def edit(date, description, amount, category):
+    """Edit an existing expense"""
+    expenses = []
     with open(expense_file, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
-        click.echo("Expense List:")
         for row in reader:
-            click.echo(f'Date: {row["Date"]}, Description: {row["Description"]}, Amount: {row["Amount"]}')
+            expenses.append(row)
+
+    for expense in expenses:
+        if expense['Date'] == date:
+            expense['Description'] = description
+            expense['Amount'] = amount
+            expense['Category'] = category
+            break
+
+    with open(expense_file, 'w', newline='') as csvfile:
+        fieldnames = ['Date', 'Description', 'Amount', 'Category']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for expense in expenses:
+            writer.writerow(expense)
+    click.echo(f'Expense updated: Date: {date}, Description: {description}, Amount: {format_currency(amount, "USD", locale="en_US")}, Category: {category}')
 
 if __name__ == '__main__':
     cli()
